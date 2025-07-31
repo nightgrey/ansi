8427ac3aedf2042dc8a763bf7e96b7ec7a4baa88
Nico <nightgrey@users.noreply.github.com>
2025-07-31 21:19:38 +0200

    Added: Vitest configuration

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from "vitest/config";
+
+export default defineConfig({
+	test: {
+      name: "ansi",
+
+      benchmark: {
+          include: ["test/**/*.bench.ts"],
+        outputJson: "test/benchmark.json",
+
+      },
+		include: ["test/**/*.test.ts"],
+    setupFiles: ["./vitest.setup.ts"],
+	},
+});
